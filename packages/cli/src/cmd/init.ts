222ddb1a452f3f6622116565e1b5c3a2334ca570
Ian Hunter <ian@users.noreply.github.com>
2023-03-19 15:13:20 -0500

    update init script client.ts

--- packages/cli/src/cmd/init.ts
+++ packages/cli/src/cmd/init.ts
@@ -94,45 +94,49 @@ There's a gitignore file in here, don't commit the following directories:
     writeFile(
       path.join(cwd, ".hybrid", "client.ts"),
       `
-// Runtime client library for Hybrid apps
+  // Runtime client library for Hybrid apps
 
-let target
-
-if (process.env.NODE_ENV === "production") {
-  // in prod, use HYBRID_ENV or default to prod
-  if (
-    process.env.HYBRID_ENV === "test" ||
-    process.env.NEXT_PUBLIC_HYBRID_ENV === "test"
-  ) {
-    target = "test"
+  let target
+  
+  if (process.env.NODE_ENV === "production") {
+    // in prod, use HYBRID_ENV or default to prod
+    if (
+      process.env.HYBRID_ENV === "test" ||
+      process.env.NEXT_PUBLIC_HYBRID_ENV === "test"
+    ) {
+      target = "test"
+    } else {
+      target = "prod"
+    }
   } else {
-    target = "prod"
+    // allow HYBRID_ENV to override dev
+    target = process.env.HYBRID_ENV || process.env.NEXT_PUBLIC_HYBRID_ENV || "dev"
   }
-} else {
-  // allow HYBRID_ENV to override dev
-  target = process.env.HYBRID_ENV || process.env.NEXT_PUBLIC_HYBRID_ENV || "dev"
-}
-
-type DeployedContract = {
-  address: string
-  chainId: number
-  txHash: string
-  blockHash: string
-  blockNumber: number
-  abi: any[]
-  bytecode: string
-}
-
-let contents = {}
-
-try {
-  contents = require(__dirname + "/" + target + ".json")
-} catch (err) {
-  console.log("Error loading deployments for target " + target)
-  console.error(err)
-}
-
-export const Deployments: Record<string, DeployedContract> = contents
+  
+  type DeployedContract = {
+    address: string
+    chainId: number
+    txHash: string
+    blockHash: string
+    blockNumber: number
+    abi: any[]
+    bytecode: string
+  }
+  
+  let contents = {}
+  
+  try {
+    if (target === "dev") {
+      contents = require("./cache/dev.json")
+    } else {
+      contents = require("./" + target + ".json")
+    }
+  } catch (err) {
+    console.log("Error loading deployments for target " + target)
+    console.error(err)
+  }
+  
+  export const Deployments: Record<string, DeployedContract> = contents
 `
     )
   })
