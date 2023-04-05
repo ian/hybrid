ef7f5a70c6278b25f5e1ebffd34483f77f16a403
Ian Hunter <ian@users.noreply.github.com>
2023-04-05 17:14:53 -0500

    export all wagmi chains

--- packages/utils/src/chains.ts
+++ packages/utils/src/chains.ts
@@ -1,16 +1,18 @@
+export * from "wagmi/chains";
+
 export function chainName(chainId: number | string) {
   switch (parseInt(chainId.toString())) {
     case 1:
-      return `Ethereum`
+      return `Ethereum`;
     case 3:
-      return `Ropsten`
+      return `Ropsten`;
     case 4:
-      return `Rinkeby`
+      return `Rinkeby`;
     case 5:
-      return `Goerli`
+      return `Goerli`;
     case 137:
-      return `Polygon`
+      return `Polygon`;
     case 80001:
-      return `Mumbai`
+      return `Mumbai`;
   }
 }
