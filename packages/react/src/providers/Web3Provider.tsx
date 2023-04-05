31075a72a3d9195d3cf67b3fd19eda07968053a0
Ian Hunter <ian@users.noreply.github.com>
2023-04-05 17:14:42 -0500

    remove semi

--- packages/react/src/providers/Web3Provider.tsx
+++ packages/react/src/providers/Web3Provider.tsx
@@ -1,28 +1,27 @@
-import { WagmiConfig, Client } from "wagmi"
+import { WagmiConfig, Client } from "wagmi";
 import {
   mainnet,
   goerli,
   localhost,
   arbitrum,
-  arbitrumGoerli
-} from "wagmi/chains"
+  arbitrumGoerli,
+} from "wagmi/chains";
 
-import React from "react"
-// import { Provider } from "@ethersproject/providers"
+import React from "react";
 import {
   ProviderKeys,
   WalletConnector,
-  WalletConnectorContext
-} from "@hybrd/types"
-import createDefaultWalletConnector from "./DefaultWalletConnector"
-import { buildProviders } from "./helpers"
+  WalletConnectorContext,
+} from "@hybrd/types";
+import createDefaultWalletConnector from "./DefaultWalletConnector";
+import { buildProviders } from "./helpers";
 
 export type Config = {
   // appName?: string
   // alchemyKey?: string
   // infuraKey?: string
   // publicProvider?: boolean
-}
+};
 
 // export const withHybrid = (InputComponent, config: Config = {}) => {
 //   return function WithHybrid(props) {
@@ -35,45 +34,45 @@ export type Config = {
 // }
 
 export function useHybridContext() {
-  return React.useContext(Web3Context)
+  return React.useContext(Web3Context);
 }
 
 export const Web3Context = React.createContext<{
-  client: Client
-  chains: any[]
-  useContext: () => WalletConnectorContext
+  client: Client;
+  chains: any[];
+  useContext: () => WalletConnectorContext;
 }>({
   client: undefined,
   chains: undefined,
-  useContext: undefined
-})
+  useContext: undefined,
+});
 
-const SUPPORTED_CHAINS = [mainnet, goerli, arbitrum, arbitrumGoerli, localhost]
+const SUPPORTED_CHAINS = [mainnet, goerli, arbitrum, arbitrumGoerli, localhost];
 
 export function Web3Provider(
   props: {
-    children: React.ReactNode
-    wallet?: WalletConnector
+    children: React.ReactNode;
+    wallet?: WalletConnector;
   } & ProviderKeys
 ) {
   const {
     children,
-    wallet: createWalletConnector = createDefaultWalletConnector
-  } = props
+    wallet: createWalletConnector = createDefaultWalletConnector,
+  } = props;
 
-  const chains = SUPPORTED_CHAINS
-  const providers = buildProviders(props)
+  const chains = SUPPORTED_CHAINS;
+  const providers = buildProviders(props);
 
   const { client, useContext, Provider } = createWalletConnector({
     chains,
-    providers
-  })
+    providers,
+  });
 
   const contextValue = {
     client,
     useContext,
-    chains
-  }
+    chains,
+  };
 
   return (
     <Web3Context.Provider value={contextValue}>
@@ -81,5 +80,5 @@ export function Web3Provider(
         <Provider>{children}</Provider>
       </WagmiConfig>
     </Web3Context.Provider>
-  )
+  );
 }
