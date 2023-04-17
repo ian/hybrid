9eca6d21cfe9ca859f1818888c322523ae2a815e
Ian Hunter <ian@users.noreply.github.com>
2023-04-17 12:05:51 -0500

    remove unused deploy code from docs

--- docs/layouts/AppLayout.tsx
+++
@@ -1,9 +0,0 @@
-import HomeHeader from "~/components/Header"
-
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<section className="flex flex-col min-h-screen">
-			<main className="flex-grow w-full p-5 space-y-5">{children}</main>
-		</section>
-	)
-}
