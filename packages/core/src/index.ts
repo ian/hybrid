062f299d828d0df8f5192cfb573d502319c1d531
Ian Hunter <ian@ianh.xyz>
2025-09-29 09:57:36 +1000

    get rid of dupe types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,7 +4,6 @@ export type { AgentConfig, DefaultRuntimeExtension } from "./core/agent"
 export { PluginRegistry } from "./core/plugin"
 export type { Plugin } from "./core/plugin"
 export { createTool, toolFactory } from "./core/tool"
-export type { Tool, ToolConfig } from "./core/tool"
 export { listen } from "./server/listen"
 export type { ListenOptions } from "./server/listen"
 
