9e94ba9c59c0c0538f976ad3274ea1612d810ae0
ian <ian@users.noreply.github.com>
2025-11-17 12:44:02 -0600

    Don't export toolFactory (#100)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,7 +3,7 @@ export { Agent } from "./core/agent"
 export type { AgentConfig, DefaultRuntimeExtension } from "./core/agent"
 export { PluginRegistry } from "./core/plugin"
 export type { Plugin } from "./core/plugin"
-export { createTool, toolFactory } from "./core/tool"
+export { createTool } from "./core/tool"
 export { listen } from "./server/listen"
 export type { ListenOptions } from "./server/listen"
 
